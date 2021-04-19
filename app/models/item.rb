3b87116174f54ce9688d2923a0cba6839bb36c60
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-04-19 11:48:35 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product
+    validates :genre_id
+    validates :text
+    validates :condition_id
+    validates :charge_id
+    validates :area_id
+    validates :day_id
+    validates :image
+    with_options format: { with: /\A[0-9]+\z/ } , numericality: {greater_than_or_equal_to: 300,less_than: 10000000} do
+      validates :price
+    end
+  end
 end
