c853788fff7da521b9486b245dfe33ce062415f0
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-04-20 19:27:42 +0900

    修正後、コメントアウト削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,6 @@ class Item < ApplicationRecord
     validates :text
     validates :image
     validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 }
-    # with_options format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 } do
-    #   validates :price
-    # end
   end
   with_options numericality: { other_than: 1 } do
     validates :genre_id
